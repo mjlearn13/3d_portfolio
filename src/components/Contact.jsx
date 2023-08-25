2c46b44442478d692cdfe3631997c087c261a25a
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2023-08-25 15:28:35 +0100

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,9 +17,45 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true);
+
+    emailjs.send(
+      'service_8a5fl3q',
+      'template_f8qkmvv',
+      {
+        from_name: form.name,
+        to_name: 'Manmeet',
+        from_email: 'form.email',
+        to_email: 'mjlearn13@gmail.com',
+        message: form.message,
+      },
+      'WsV491trcA2-zvAYr'
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank you. I will get back to you as soon as possible.');
+
+      setForm({
+        name:'',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+
+      console.log(error);
+      
+      alert('Something went wrong.')
+    })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
